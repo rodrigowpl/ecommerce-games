eb23b85d9c8b1a3d1689b8805d7a3cbd068f3d47
Rodrigo Wippel <rodrigow1992@gmail.com>
2020-08-30 19:55:53 -0300

    client -> jest and testing library setup

---
+++ client/src/components/Main/index.tsx
@@ -0,0 +1,6 @@
+const Main = () => (
+  <main>
+    <h2>React Avançado</h2>
+  </main>
+)
+export default Main
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1>Hey there</h1>
     </div>
   )
 }
