ab116d0c3c57f44a7a159d8635f66bc49a4ec710
Rodrigo Wippel <rodrigow1992@gmail.com>
2020-08-30 21:58:35 -0300

    main page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,13 +1,5 @@
-import Head from 'next/head'
+import Main from 'components/Main'
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Hey there</h1>
-    </div>
-  )
+  return <Main />
 }
