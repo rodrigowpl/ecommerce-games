92bbb7da52fd81681a59ba020bffa1ce70359481
Rodrigo Wippel <rodrigow1992@gmail.com>
2020-08-30 23:12:02 -0300

    storybook

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -3,8 +3,6 @@ module.exports = {
     "../src/components/**/stories.tsx"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-actions",
     "@storybook/addon-essentials"
   ]
 }
--- client/src/components/Main/stories.tsx
+++ client/src/components/Main/stories.tsx
@@ -1,8 +1,10 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main
-}
+} as Meta
 
-export const Basic = () => <Main />
+export const Basic: Story = () => <Main />
