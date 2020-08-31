1ce3974aeb14f16e5a2b04098ada6b559e224a1e
Rodrigo Wippel <rodrigow1992@gmail.com>
2020-08-30 23:19:13 -0300

    pwa setup

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -10,6 +10,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <title>React Advanced NextJS Boilerplace</title>
         <link rel="shortcut icon" href="/img/icon-192.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A boilerplace app starter to work with TypeScript, React, NextJS and Styled Components"
